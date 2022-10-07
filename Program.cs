b9516aa7a1294d516b990cf548bd0fbb8f51e553
Elijah Kariuki <ElijahKariuki1981@outlook.com>
2022-10-07 09:27:55 -0400

    initial commit

---
+++ Program.cs
@@ -0,0 +1,2 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("God You are Faithful. \nYou were faithful yesterday, \nYou are faithful today, \nand You will be faithful tomorrow!!\nGreat is Your faithfulness to me!!");
